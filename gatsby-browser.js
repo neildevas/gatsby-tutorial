3e75b071a09970922f42f53fcefe7f88024bd24a
Neil Devas <neil.devas97@gmail.com>
2020-10-26 10:04:36 -0400

    Gatsby tutorial

---
+++ gatsby-browser.js
@@ -0,0 +1,4 @@
+// gatsby-browser.js is a special file name.
+// The tool is configured so that Gatsby will look specifically for this file, and implicitely know what to do with it.
+// That means, less code for us!
+import './src/styles/global.css';
