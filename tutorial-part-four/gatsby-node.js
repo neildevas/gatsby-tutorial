302a8364de0b98d82e5283f32afbad3c45418b90
Neil Devas <neil.devas97@gmail.com>
2020-10-27 09:26:27 -0400

    Finish creating blog

--- tutorial-part-four/gatsby-node.js
+++ tutorial-part-four/gatsby-node.js
@@ -1,12 +1,43 @@
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require(`path`);
+
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` });
     actions.createNodeField({
       node,
-      name: `postSlug`,
+      name: 'slug',
       value: slug,
     })
   }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    })
+  });
+  console.log(JSON.stringify(result, null, 4))
 };
